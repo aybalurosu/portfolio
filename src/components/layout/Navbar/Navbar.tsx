6155cc9a507cac7af7bbc90256f4d6f091c85e50
aybaluro <aybalurosu@gmail.com>
2025-09-18 15:12:45 +0200

    navbar changes

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -2,8 +2,8 @@ import "../../../app/globals.css";
 
 export function Navbar() {
   return (
-    <nav className="backdrop-blur-3xl rounded-full">
-      <ul className="inline-flex justify-between p-4">
+    <nav className="backdrop-blur-lg rounded-full bg-[rgba(255,255,255,0.1)] border border-[rgba(255,255,255,0.11)] shadow-lg shadow-[rgba(0,0,0,0.2)]">
+      <ul className="inline-flex gap-7 p-4">
         <li><a href="#hero" className="text-[var(--text-color)] p-2 px-4 rounded-full border-1 border-transparent transition-all hover:text-[var(--hover-text-color)] hover:border-amber-50">Home</a></li>
         <li><a href="#about"  className="text-[var(--text-color)] p-2 px-4 rounded-full border-1 border-transparent transition-all hover:text-[var(--hover-text-color)] hover:border-amber-50">About</a></li>
         <li><a href="#skills"  className="text-[var(--text-color)] p-2 px-4 rounded-full border-1 border-transparent transition-all hover:text-[var(--hover-text-color)] hover:border-amber-50">Skills</a></li>
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,8 +2,7 @@
 export function Hero() {
   return (
     <section id="hero" className="flex flex-col lg:flex-row md:flex-row items-center justify-center gap-4
-    min-h-screen w-full
-    ">
+    min-h-screen w-full">
       <h1>I&apos;m Alexinia Balutescu Rosu</h1>
     </section>
   );
